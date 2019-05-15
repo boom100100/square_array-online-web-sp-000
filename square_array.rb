8c330aa6201624428c5a63904316550dd349207d
Bernadette Davis <bsd231@nyu.edu>
2019-05-15 13:44:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,4 @@
 def square_array(array)
   # your code here
-  array.each{ |element|
-    array[element] = element * element
-    
-  }
+  array.each{ |element| array[element] = element * element }
 end
